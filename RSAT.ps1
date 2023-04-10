1db6192a12a06973b9d9ef2bfeffb4ccd3c03a11
Andres Burgos <93954380+Leonso20@users.noreply.github.com>
2023-04-10 12:17:14 -0400

    Add files via upload

---
+++ RSAT.ps1
@@ -0,0 +1,17 @@
+﻿<#Use the following command to establish a remote PowerShell session to the target computer:
+Replace <ComputerName> with the name of the remote computer and <UserName> with the username and password that has administrative rights on the remote computer.
+Once you are connected to the remote computer, use the following command to install the RSAT tools:
+#>
+
+Enter-PSSession -ComputerName <ComputerName> -Credential <UserName>
+
+
+#This command will install all available RSAT tools on the remote computer
+Get-WindowsCapability -Online | Where-Object { $_.Name -like 'RSAT*' } | Add-WindowsCapability -Online
+
+
+#Once the installation is complete, you can exit the remote PowerShell session by using the following command:
+
+Exit-PSSession
+
+#That's it! You have now installed RSAT remotely on the target computer using PowerShell.
