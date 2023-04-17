dbb4e1313dfc97cbc2b6c19668b96248c75c3cc9
Andres Burgos <93954380+Leonso20@users.noreply.github.com>
2023-04-17 08:03:57 -0400

    Add files via upload
    This script uses a while loop to continuously call the Show-ResourceUsage function, which retrieves and displays the system resource usage (CPU usage, memory usage, and disk usage) in real-time. The Start-Sleep cmdlet is used to pause the script for 1 second between each iteration of the loop, so that the resource usage data is updated every second. You can stop the script by pressing Ctrl + C in the PowerShell console.

---
+++ monitors system resources .ps1
@@ -0,0 +1,45 @@
+﻿<#PowerShell script that monitors system resources (CPU usage, memory usage, and disk usage) in real-time and displays the data continuously:#>
+
+
+
+# Function to get total CPU usage percentage
+Function Get-TotalCpuUsage {
+    $cpuUsage = Get-Counter '\Processor(_Total)\% Processor Time' -ErrorAction SilentlyContinue |
+                Select-Object -ExpandProperty CounterSamples |
+                Select-Object -ExpandProperty CookedValue
+    $cpuUsage
+}
+
+# Function to get total memory usage percentage
+Function Get-TotalMemoryUsage {
+    $memoryUsage = Get-Counter '\Memory\% Committed Bytes In Use' -ErrorAction SilentlyContinue |
+                   Select-Object -ExpandProperty CounterSamples |
+                   Select-Object -ExpandProperty CookedValue
+    $memoryUsage
+}
+
+# Function to get total disk usage percentage
+Function Get-TotalDiskUsage {
+    $diskUsage = Get-Counter '\LogicalDisk(_Total)\% Free Space' -ErrorAction SilentlyContinue |
+                 Select-Object -ExpandProperty CounterSamples |
+                 Select-Object -ExpandProperty CookedValue |
+                 ForEach-Object { 100 - $_ }
+    $diskUsage
+}
+
+# Function to display system resource usage
+Function Show-ResourceUsage {
+    $cpuUsage = Get-TotalCpuUsage
+    $memoryUsage = Get-TotalMemoryUsage
+    $diskUsage = Get-TotalDiskUsage
+
+    Write-Host "`nCPU Usage: $cpuUsage%"
+    Write-Host "Memory Usage: $memoryUsage%"
+    Write-Host "Disk Usage: $diskUsage%"
+}
+
+# Continuously display system resource usage every 1 second
+while ($true) {
+    Show-ResourceUsage
+    Start-Sleep -Seconds 1
+}
